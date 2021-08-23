545b2b55bf704eeb0360bb15b8bcb43a5244883a
あきみど <65846606+AKI-MIDO@users.noreply.github.com>
2021-08-23 10:58:31 +0900

    Add files via upload

---
+++ Atcorder.py
@@ -0,0 +1,19 @@
+#競技プログラミングやコーディングテストなどで使う標準入力
+
+#入力　整数1個:100
+N=int(input())
+
+#入力　１行に要素n個
+s1,s2,s3=input().split() #文字列
+
+s4,s5,s6=map(int, input().split()) #数字
+
+s=[input() for i in range(N)] #aaa
+                              #bbb
+                              #ccc
+
+n=[int(input()) for i in range(N)] #1
+                                   #2
+                                   #3
+
+
